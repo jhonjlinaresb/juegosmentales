6b16b8fdb3724d361794edc91ff0f1c64eb25f59
unknown <jhonjlinaresb@gmail.com>
2021-09-15 00:06:22 +0200

    Este humilde programador se va a dormir luego de no conseguir el objetivo por hoy

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,26 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {BrowserRouter, Link, Route} from "react-router-dom";
 import brain from '../brain.svg';
 
+import Menu from "./menu";
+
 function Home() {
+   const handleClick = (onClick) => {
+       <Menu />
+   }
+    
     return (
+        
         <div className="App">
         <h6>Juegos Mentales</h6>
         <img src={brain} className="brain" alt="brain" />
         <p>
           Prepárate para alimentar tu mente...
         </p>
-        <Link to="/menu">
-        <button>Iniciar</button>
-        </Link>
+        
+        <BrowserRouter>
+        <button onClick={handleClick}>Iniciar</button>
+        </BrowserRouter>
         </div>
     );
     }
